aa54cb1f3dd1b7ca5a19bdeaf422e7c6b5389fa8
Hugo Jeller <hugo_jeller@hotmail.com>
2019-12-02 05:51:34 +0000

    added paypal wrapper logs

--- app/Classes/PayPalWrapper.php
+++ app/Classes/PayPalWrapper.php
@@ -8,6 +8,7 @@
 
 namespace App\Classes;
 
+use Illuminate\Support\Facades\Log;
 use Srmklive\PayPal\Services\ExpressCheckout;
 
 class PayPalWrapper extends MockableWrapper
@@ -30,21 +31,29 @@ class PayPalWrapper extends MockableWrapper
 
 	public static function _setExpressCheckout($cart)
 	{
+		Log::info("_setExpressCheckout called");
+
 		return static::getInstance()->setExpressCheckout($cart);
 	}
 
 	public static function _getExpressCheckoutDetails($token)
 	{
+		Log::info("_getExpressCheckoutDetails called");
+
 		return static::getInstance()->getExpressCheckoutDetails($token);
 	}
 
 	public static function _doExpressCheckoutPayment($cart, $token, $payerId)
 	{
+		Log::info("_doExpressCheckoutPayment called");
+
 		return static::getInstance()->doExpressCheckoutPayment($cart, $token, $payerId);
 	}
 
 	public static function _getTransactionDetails($transactionId)
 	{
+		Log::info("_getTransactionDetails called");
+
 		return static::getInstance()->getTransactionDetails($transactionId);
 	}
 }
