23cd91a4b7dfe05c02421d642191addd110e827c
Hugo Jeller <hugo_jeller@hotmail.com>
2020-01-07 10:54:46 -0400

    Added logs to PayPal initialization

--- app/Services/PayPalOrderService.php
+++ app/Services/PayPalOrderService.php
@@ -16,73 +16,77 @@ use Exception;
 class PayPalOrderService
 {
 
-	/**
-	 * @param Order $order
-	 *
-	 * @return array
-	 */
-	public static function getCheckoutCart(Order $order)
-	{
-		$id = $order->id;
-		$prefix = config('paypal.invoice_prefix');
+    /**
+     * @param Order $order
+     *
+     * @return array
+     */
+    public static function getCheckoutCart(Order $order)
+    {
+        $id = $order->id;
+        $prefix = config('paypal.invoice_prefix');
         $price = round($order->preset_amount / 100, 2);
 
-		// Set checkout options
-		$data['items'] = [[
-			'name'  => $order->reason,
-			'price' => $price,
-			'qty'   => 1,
-		]];
-		$data['invoice_id'] = "{$prefix}_{$id}";
-		$data['invoice_description'] = "Pedido #$id";
-		$data['return_url'] = route('orders.show', [$order, 'pending']);
-		$data['cancel_url'] = route('orders.show', [$order, 'cancel']);
-		$data['total'] = $price;
+        // Set checkout options
+        $data['items'] = [[
+            'name' => $order->reason,
+            'price' => $price,
+            'qty' => 1,
+        ]];
+        $data['invoice_id'] = "{$prefix}_{$id}";
+        $data['invoice_description'] = "Pedido #$id";
+        $data['return_url'] = route('orders.show', [$order, 'pending']);
+        $data['cancel_url'] = route('orders.show', [$order, 'cancel']);
+        $data['total'] = $price;
 
-		return $data;
-	}
+        return $data;
+    }
 
-	/**
-	 * @param $order
-	 *
-	 * @throws Exception
-	 */
-	public function initialize($order)
-	{
-		// Create order database entries
-		$ppOrder = PayPalOrder::create();
+    /**
+     * @param $order
+     *
+     * @throws Exception
+     */
+    public function initialize($order)
+    {
+        // Create order database entries
+        $ppOrder = PayPalOrder::create();
 
-		// Associate details
-		$ppOrder->base()->save($order);
-		info('PayPal details saved');
-		$order->save();
-		info('Order details saved');
+        // Associate details
+        $ppOrder->base()->save($order);
+        info('PayPal details saved');
+        $order->save();
+        info('Order details saved');
 
-		// Process checkout cart
-		$cart = $this->getCheckoutCart($order);
+        // Process checkout cart
+        $cart = $this->getCheckoutCart($order);
 
-		// Request PayPal checkout token
-		info('Setting ExpressCheckout.');
-		$response = PayPalWrapper::setExpressCheckout($cart);
-		info('ExpressCheckout set.', compact('response'));
+        // Request PayPal checkout token
+        info('Setting ExpressCheckout.');
+        $response = PayPalWrapper::setExpressCheckout($cart);
+        info('ExpressCheckout set.', compact('response'));
 
-		// Check if response is valid
-		if (!is_array($response))
-			throw new Exception('Invalid response from PayPal');
+        // Check if response is valid
+        if (!is_array($response)) {
+            info('Invalid response from PayPal', compact('response'));
+            throw new Exception('Invalid response from PayPal');
+        }
 
-		// Check if token was returned
-		if (!array_key_exists('TOKEN', $response))
-			throw new Exception('PayPal did not return a token');
+        // Check if token was returned
+        if (!array_key_exists('TOKEN', $response)) {
+            info('PayPal did not return a token', compact('response'));
+            throw new Exception('PayPal did not return a token');
+        }
 
-		// Store token and base Order
-		$ppOrder->token = $response['TOKEN'];
-		$ppOrder->link = $response['paypal_link'];
-		$ppOrder->save();
+        // Store token and base Order
+        $ppOrder->token = $response['TOKEN'];
+        $ppOrder->link = $response['paypal_link'];
+        $ppOrder->save();
 
-	}
+    }
 
-	public function recheck(PayPalOrder $order)
-	{
+    public function recheck(PayPalOrder $order)
+    {
 
         // Check if PayPal has checkout details
         $response = PayPalWrapper::getExpressCheckoutDetails($order->token);
@@ -124,5 +128,5 @@ class PayPalOrderService
             $order->base->paid_amount = $order->base->preset_amount;
             $order->base->save();
         }
-	}
+    }
 }
