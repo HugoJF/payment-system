4a97214d7296dfe84f10b7b4e67f85a40d0b01f2
Hugo Jeller <hugo_jeller@hotmail.com>
2020-01-07 10:50:51 -0400

    Refactored PayPal cart data generation

--- app/Services/PayPalOrderService.php
+++ app/Services/PayPalOrderService.php
@@ -23,26 +23,21 @@ class PayPalOrderService
 	 */
 	public static function getCheckoutCart(Order $order)
 	{
-		$order_id = $order->id;
+		$id = $order->id;
+		$prefix = config('paypal.invoice_prefix');
+        $price = round($order->preset_amount / 100, 2);
 
 		// Set checkout options
 		$data['items'] = [[
 			'name'  => $order->reason,
-			'price' => round($order->preset_amount / 100, 2),
+			'price' => $price,
 			'qty'   => 1,
 		]];
-		$data['invoice_id'] = config('paypal.invoice_prefix') . '_' . $order_id;
-		$data['invoice_description'] = "Pedido #$order_id";
+		$data['invoice_id'] = "{$prefix}_{$id}";
+		$data['invoice_description'] = "Pedido #$id";
 		$data['return_url'] = route('orders.show', [$order, 'pending']);
-		$data['cancel_url'] = url("orders/{$order->id}/cancel");
-
-		// Calculate total price
-		$total = collect($data['items'])->reduce(function ($total, $item) {
-			return $total + $item['price'] * $item['qty'];
-		}, 0);
-
-		// Update price on preferences
-		$data['total'] = round($total, 2);
+		$data['cancel_url'] = route('orders.show', [$order, 'cancel']);
+		$data['total'] = $price;
 
 		return $data;
 	}
