412fb2e2d9b5f37ded5215e579a314fea9216cd6
Hugo Jeller <hugo_jeller@hotmail.com>
2020-01-28 16:46:12 +0000

    Refactored RefreshItemData command

--- app/Console/Commands/RefreshItemData.php
+++ app/Console/Commands/RefreshItemData.php
@@ -5,103 +5,95 @@ namespace App\Console\Commands;
 use App\SteamItem;
 use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 use Ixudra\Curl\Facades\Curl;
 
 class RefreshItemData extends Command
 {
-	/**
-	 * The name and signature of the console command.
-	 *
-	 * @var string
-	 */
-	protected $signature = 'items:refresh {tfa}';
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Generates fresh data from BitSkins API';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'items:refresh {tfa}';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generates fresh data from BitSkins API';
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    protected $blacklistedWords = ['Sticker', 'Holo', 'Foil', 'Gloves', 'Souvenir'];
 
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function handle()
-	{
-		$rawBsData = $this->getBitSkinsData($this->argument('tfa'));
-		$bsData = collect($rawBsData->prices);
-		$this->info("Collected {$bsData->count()} prices");
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $rawBsData = $this->getBitSkinsData($this->argument('tfa'));
+        $bsData = collect($rawBsData->prices);
+        $this->info("Collected {$bsData->count()} prices");
 
+        SteamItem::truncate();
 
-		SteamItem::truncate();
+        $bsData = $bsData->reject(function ($item) {
+            return $this->isItemBlacklisted($item->market_hash_name);
+        });
 
-		$bsData = $bsData->reject(function ($item) {
-			return
-				str_contains($item->market_hash_name, 'Sticker') ||
-				str_contains($item->market_hash_name, 'Holo') ||
-				str_contains($item->market_hash_name, 'Foil') ||
-				str_contains($item->market_hash_name, 'Gloves') ||
-				str_contains($item->market_hash_name, 'Souvenir');
-		});
+        $this->info("After filtering: {$bsData->count()}");
 
-		$this->info("After filtering: {$bsData->count()}");
+        $bsData->each(function ($item) {
+            preg_match('/(★ )?(StatTrak™)? ?(.*?) \| (.*?) \((.*?)\)/', $item->market_hash_name, $matches);
+            $i = SteamItem::make();
+            $i->fill((array) $item);
+            $i->price = ceil($item->price * 100);
+            try {
+                if ($matches[2]) {
+                    $i->stattrak = true;
+                } else {
+                    $i->stattrak = false;
+                }
+                $i->item_name = trim(($matches[1] ?? '') . ' ' . $matches[3]);
+                $i->skin_name = $matches[4];
+                $i->condition = $matches[5];
+            } catch (Exception $e) {
+                logger()->warning("Error while splitting {$item->market_hash_name}");
+            }
 
-		$bsData->each(function ($item) {
-			preg_match('/(★ )?(StatTrak™)? ?(.*?) \| (.*?) \((.*?)\)/', $item->market_hash_name, $matches);
-			$i = SteamItem::make();
-			$i->fill((array) $item);
-			$i->price = ceil($item->price * 100);
-			try {
-				if ($matches[2]) {
-					$i->stattrak = true;
-				} else {
-					$i->stattrak = false;
-				}
-				$i->item_name = trim(($matches[1] ?? '') . ' ' . $matches[3]);
-				$i->skin_name = $matches[4];
-				$i->condition = $matches[5];
-			} catch (Exception $e) {
-				logger()->warning("Error while splitting {$item->market_hash_name}");
-			}
+            try {
+                $i->save();
+            } catch (Exception $e) {
+                $message = $e->getMessage();
+                $code = $e->getCode();
+                $this->error("Error while saving item {$item->market_hash_name}: [$code] $message");
 
-			try {
-				$i->save();
-			} catch (Exception $e) {
-			    $message = $e->getMessage();
-			    $code = $e->getCode();
-				$this->error("Error while saving item {$item->market_hash_name}: [$code] $message");
+                logger()->warning("Error while saving item {$item->market_hash_name}", [
+                    'item' => $item,
+                ]);
+            }
+        });
+    }
 
-				logger()->warning("Error while saving item {$item->market_hash_name}", [
-					'item' => $item,
-				]);
-			}
-		});
-	}
+    protected function isItemBlacklisted($itemName)
+    {
+        return Str::contains($itemName, $this->blacklistedWords);
+    }
 
-	private function getBitSkinsData($tfa)
-	{
-		return cache()->remember('bit-skins-api', 60000, function () use ($tfa) {
-			$api = config('payment-system.bit-skins-key');
-			$result = Curl::to("https://bitskins.com/api/v1/get_all_item_prices/");
-			$result->withData([
-				'api_key' => $api,
-				'code'    => $tfa,
-				'app_id'  => 730,
-			]);
-			$result->asJson();
+    private function getBitSkinsData($tfa)
+    {
+        return cache()->remember('bit-skins-api', 60000, function () use ($tfa) {
+            $api = config('payment-system.bit-skins-key');
+            $result = Curl::to("https://bitskins.com/api/v1/get_all_item_prices/");
+            $result->withData([
+                'api_key' => $api,
+                'code'    => $tfa,
+                'app_id'  => 730,
+            ]);
+            $result->asJson();
 
-			return $result->get();
-		});
-	}
+            return $result->get();
+        });
+    }
 }
